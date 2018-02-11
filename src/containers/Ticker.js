25ae6c14f496f67b670cdcce7a5354500c692bd3
jugalthakkar <jugal.thakkar@gmail.com>
2018-02-12 00:17:49 +0530

    more styling

--- src/containers/Ticker.js
+++ src/containers/Ticker.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Websocket from 'react-websocket';
-import {Header, Icon, Table} from 'semantic-ui-react';
+import {Container, Header, Icon, Table} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import TickerName from '../components/TickerName';
 import TickerPrice from '../components/TickerPrice';
@@ -54,28 +54,29 @@ class Ticker extends Component {
                                positive={this.state.tickers[key].price > this.state.tickers[key].previousPrice}
                                negative={this.state.tickers[key].price < this.state.tickers[key].previousPrice}
                     >
-                        <Table.Cell>
+                        <Table.Cell textAlign="center">
                             <Header as='h3'><TickerName name={key}/></Header>
                         </Table.Cell>
-                        <Table.Cell>
+                        <Table.Cell textAlign="center">
                             <TickerPrice price={this.state.tickers[key].price}
                                          previousPrice={this.state.tickers[key].previousPrice}/>
                             <TickerTime time={this.props.updateTime}/>
                         </Table.Cell>
-                        <Table.Cell>
+                        <Table.Cell textAlign="center">
                             <TickerHistory priceHistory={this.state.tickers[key].priceHistory}/>
                         </Table.Cell>
                     </Table.Row>
                 )
             );
         return (
-            <div className="Ticker">
+
+            <Container>
                 <Table stackable padded>
                     <Table.Header>
                         <Table.Row>
-                            <Table.HeaderCell>Name</Table.HeaderCell>
-                            <Table.HeaderCell><Icon name="dollar"/> Price</Table.HeaderCell>
-                            <Table.HeaderCell><Icon name="line chart"/> History</Table.HeaderCell>
+                            <Table.HeaderCell textAlign="center">Name</Table.HeaderCell>
+                            <Table.HeaderCell textAlign="center"><Icon name="dollar"/> Price</Table.HeaderCell>
+                            <Table.HeaderCell textAlign="center"><Icon name="line chart"/> History</Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
@@ -84,7 +85,7 @@ class Ticker extends Component {
                 </Table>
                 <Websocket url={this.props.url}
                            onMessage={this.updateTickers.bind(this)}/>
-            </div>
+            </Container>
         );
     }
 }
