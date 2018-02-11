16b22cefcd3bbd6c43cc73a58c7cddbfd09b4fd7
jugalthakkar <jugal.thakkar@gmail.com>
2018-02-11 13:43:06 +0530

    end 2 end stable version - no css

--- src/App.js
+++ src/App.js
@@ -2,19 +2,9 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class TickerHeader extends Component {
-    render() {
-        return (<header className="Ticker-header">
-            <div>Name</div>
-            <div>Price</div>
-            <div>Update</div>
-        </header>);
-    }
-}
-
 class TickerName extends Component {
     render() {
-        return (<span className="Ticker-name">{this.props.name}</span>);
+        return (<span className="Ticker-name">{this.props.name.toUpperCase()}</span>);
     }
 }
 
@@ -32,53 +22,85 @@ class TickerTime extends Component {
 
 class TickerRow extends Component {
     render() {
-        return (<div className="Ticker-row">
-            <div className="Ticker-name-container"><TickerName name={this.props.name}/></div>
-            <div className="Ticker-price-container"><TickerPrice price={this.props.price}/></div>
-            <div className="Ticker-time-container"><TickerTime time={new Date()}/></div>
+        let name, price, time;
+        if (this.props.header) {
+            name = "Name";
+            price = "Price";
+            time = "Update Time";
+        } else {
+            name = (<TickerName name={this.props.name}/>);
+            price = (<TickerPrice price={this.props.price}/>);
+            time = (<TickerTime time={this.props.updateTime}/>);
+        }
+        return (<div className={"Ticker-row" + (this.props.header ? " Ticker-header" : "")}>
+            <div className="Ticker-name-container">{name}</div>
+            <div className="Ticker-price-container">{price}</div>
+            <div className="Ticker-time-container">{time}</div>
         </div>);
     }
 }
 
 
 class Ticker extends Component {
+
+    updateTickers(data) {
+        const tickers = [];
+        data.forEach(([name, price]) => {
+            tickers[name] = {price, updateTime: new Date()};
+        });
+        Object.entries(this.state.tickers).forEach(([name, value]) => {
+            if (!tickers[name]) {
+                tickers[name] = value;
+            }
+        });
+        this.setState({tickers});
+    }
+
     constructor(props) {
         super(props);
-        this.state = [{
-            name: 'AAPL',
-            price: 50
-        }, {
-            name: 'GOOG',
-            price: 80
-        }];
+        this.state = {
+            tickers: {}
+        };
+        const webSocket = new WebSocket(this.props.url);
 
-        // var exampleSocket = new WebSocket(this.props.url);
+        webSocket.onmessage = (event) => {
+            this.updateTickers(JSON.parse(event.data));
+        }
     }
 
     render() {
+        const tickerRows = Object
+            .keys(this.state.tickers)
+            .sort()
+            .map(key =>
+                (<TickerRow
+                    key={key}
+                    name={key}
+                    updateTime={this.state.tickers[key].updateTime}
+                    price={this.state.tickers[key].price}/>)
+            );
         return (
             <div className="Ticker">
-                <TickerHeader/>
-                {this.state.map(tickerItem =>
-                    <TickerRow key={tickerItem.name} name={tickerItem.name} price={tickerItem.price} />
-                )}
+                <TickerRow header={true}/>
+                {tickerRows}
             </div>
         );
     }
 }
 
-class App extends Component {
+class App
+    extends Component {
     render() {
         return (
             <div className="App">
-                {/*<header className="App-header">*/}
-                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-                    {/*<h1 className="App-title">Welcome to React</h1>*/}
-                {/*</header>*/}
+                <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Welcome to React</h1>
+                </header>
                 <Ticker url="ws://stocks.mnet.website"/>
-                {/*<p className="App-intro">*/}
-                    {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-                {/*</p>*/}
+                <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
             </div>
         );
     }
