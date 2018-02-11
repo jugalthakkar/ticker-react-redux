abb3311989022dbb4ae52126bc74e71b35b2010a
jugalthakkar <jugal.thakkar@gmail.com>
2018-02-11 17:19:02 +0530

    added sparklines, icons and some basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Moment from 'react-moment';
+import FontAwesome from 'react-fontawesome';
+import {Sparklines, SparklinesLine, SparklinesSpots} from 'react-sparklines';
 
 class TickerName extends Component {
     render() {
@@ -9,14 +12,55 @@ class TickerName extends Component {
 }
 
 class TickerPrice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lastPrice: undefined,
+            priceHistory: []
+        };
+    }
+
     render() {
-        return (<span className="Ticker-price">{this.props.price}</span>);
+        let changeClassName = 'price-same', iconName = 'minus';
+        if (this.state.lastPrice) {
+            if (this.props.price < this.state.lastPrice) {
+                changeClassName = 'price-down';
+                iconName = 'angle-double-down';
+            } else if (this.props.price > this.state.lastPrice) {
+                changeClassName = 'price-up';
+                iconName = 'angle-double-up';
+            }
+        }
+        return (<div className={changeClassName + " Ticker-price"}>
+            <span className="price">{this.props.price.toFixed(3)}</span>
+            <span className="symbol"><FontAwesome name={iconName}/></span>
+            <span className="price">
+                <Sparklines data={[...this.state.priceHistory, this.props.price]} limit={5}>
+                    <SparklinesLine color="#1c8cdc"/>
+                    <SparklinesSpots/>
+                </Sparklines>
+            </span>
+        </div>);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.time > this.props.time) {
+            const lastPrice = this.props.price;
+            this.setState((prevState, props) => {
+                return {
+                    priceHistory: [...prevState.priceHistory, lastPrice],
+                    lastPrice
+                };
+            });
+        }
     }
 }
 
+
 class TickerTime extends Component {
     render() {
-        return (<span className="Ticker-time">{this.props.time.toLocaleTimeString()}</span>);
+
+        return (<span className="Ticker-time"><Moment fromNow>{this.props.time}</Moment></span>);
     }
 }
 
@@ -26,10 +70,10 @@ class TickerRow extends Component {
         if (this.props.header) {
             name = "Name";
             price = "Price";
-            time = "Update Time";
+            time = "Last Update";
         } else {
             name = (<TickerName name={this.props.name}/>);
-            price = (<TickerPrice price={this.props.price}/>);
+            price = (<TickerPrice price={this.props.price} time={this.props.updateTime}/>);
             time = (<TickerTime time={this.props.updateTime}/>);
         }
         return (<div className={"Ticker-row" + (this.props.header ? " Ticker-header" : "")}>
@@ -40,26 +84,31 @@ class TickerRow extends Component {
     }
 }
 
-
 class Ticker extends Component {
 
     updateTickers(data) {
-        const tickers = [];
-        data.forEach(([name, price]) => {
-            tickers[name] = {price, updateTime: new Date()};
+        this.setState((prevState, props) => {
+            const tickers = [];
+            data.forEach(([name, price]) => {
+                tickers[name] = {price, updateTime: new Date()};
+            });
+            Object.entries(prevState.tickers).forEach(([name, value]) => {
+                if (!tickers[name]) {
+                    tickers[name] = value;
+                }
+            });
+            return {
+                counter: prevState.counter + 1,
+                tickers
+            };
         });
-        Object.entries(this.state.tickers).forEach(([name, value]) => {
-            if (!tickers[name]) {
-                tickers[name] = value;
-            }
-        });
-        this.setState({tickers});
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            tickers: {}
+            tickers: {},
+            counter: 0
         };
         const webSocket = new WebSocket(this.props.url);
 
@@ -88,19 +137,15 @@ class Ticker extends Component {
     }
 }
 
-class App
-    extends Component {
+class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">Welcome to React</h1>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <Ticker url="ws://stocks.mnet.website"/>
-                <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
             </div>
         );
     }
